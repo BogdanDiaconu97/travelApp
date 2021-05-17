63397090cfa0f1b0f190e50694a625d12d84779f
Bogdan Diaconu <diaconu.bogdan@outlook.com>
2021-05-17 21:47:46 +0300

    refactoring: Added <nav-bar> lit component + functionality onClick <h1>

--- src/MyComponents/navBar.js
+++ src/MyComponents/navBar.js
@@ -1,19 +1,94 @@
 import { LitElement, html, css } from 'lit-element';
+import '../LionWebComponents/myLionButton.js';
 
 class NavBar extends LitElement {
   static get styles() {
-    return css``;
+    return css`
+      nav {
+        padding: 16px 0 32px 0;
+      }
+      a {
+        text-decoration: none;
+        color: #fff;
+      }
+      h1:hover {
+        color: lightgrey;
+        cursor: pointer;
+        color: #3a954a;
+      }
+      ul {
+        display: none;
+        padding: 0px;
+      }
+      li {
+        display: block;
+        padding: 12px 0;
+      }
+      a:hover {
+        color: #3a954a;
+        font-weight: bold;
+      }
+      my-lion-button {
+        border: 1px solid black;
+        border-radius: 8px;
+        font-weight: bold;
+        color: #6b48a8;
+        background-color: #3a954a;
+      }
+      my-lion-button:hover {
+        color: #3a954a;
+        background-color: #6b48a8;
+        cursor: pointer;
+      }
+      @media only screen and (min-width: 768px) {
+        nav {
+          display: flex;
+          justify-content: space-between;
+        }
+        ul {
+          display: block;
+          padding-right: 32px;
+        }
+        h1 {
+          padding-left: 32px;
+        }
+        li {
+          display: inline-block;
+          padding: 0px 8px;
+        }
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      _isRendering: Boolean,
+    };
   }
 
   connectedCallback() {
     super.connectedCallback();
   }
 
+  _handleClick(event) {
+    const ul = event.target.nextElementSibling;
+    console.log();
+    if (this._isRendering) {
+      ul.style.display = 'none';
+      this._isRendering = false;
+    } else {
+      ul.style.display = 'block';
+      this._isRendering = true;
+    }
+  }
+
   render() {
     return html`
       <header>
         <nav>
-          <h1>My Travel App ✌</h1>
+          <h1 @click=${this._handleClick} @keypress=${this._handleClick}>
+            My Travel App ✌
+          </h1>
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">Tour</a></li>
@@ -21,9 +96,7 @@ class NavBar extends LitElement {
             <li><a href="#">Places</a></li>
             <li><a href="#">Contact Us</a></li>
             <li>
-              <a href="#"
-                ><button type="button" class="bookNow">Book now</button></a
-              >
+              <my-lion-button type="button"> Book now </my-lion-button>
             </li>
           </ul>
         </nav>
