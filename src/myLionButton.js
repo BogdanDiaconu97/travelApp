3fcba2db3e2ae981fad1c4be544ef58c08342d18
Bogdan Diaconu <diaconu.bogdan@outlook.com>
2021-05-20 13:21:01 +0300

    Deleted unnecessary files from src folder

--- src/myLionButton.js
+++
@@ -1,9 +0,0 @@
-import { LionButton } from '@lion/button';
-
-class MyLionButton extends LionButton {
-  static get styles() {
-    return super.styles;
-  }
-}
-
-customElements.define('my-lion-button', MyLionButton);
--- src/myLionInput.js
+++
@@ -1,9 +0,0 @@
-import { LionInput } from '@lion/input';
-
-class MyLionInput extends LionInput {
-  static get styles() {
-    return super.styles;
-  }
-}
-
-customElements.define('my-lion-input', MyLionInput);
